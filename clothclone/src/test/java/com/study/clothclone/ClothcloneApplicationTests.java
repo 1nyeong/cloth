274484ab6b0558d2b670c07d8fbf1328834c2fcd
D:\hgit <inyeong0726@gmail.com>
2022-10-14 04:11:03 +0900

    cloth1

---
+++ clothclone/src/test/java/com/study/clothclone/ClothcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.clothclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
