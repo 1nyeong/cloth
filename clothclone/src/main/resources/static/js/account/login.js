15fd4702c95f33a6b98306e07811f63e1c0afd3e
1nyeong <inyeong0726@gmail.com>
2022-11-02 13:27:47 +0900

    login.js 수정

--- clothclone/src/main/resources/static/js/account/login.js
+++ clothclone/src/main/resources/static/js/account/login.js
@@ -1,14 +1,5 @@
-const registerForwordButton = document.querySelectorAll(".account-button")[1];
+const registerForwardButton = document.querySelectorAll(".account-button")[1];
 
-registerForwordButton.onclick = () => {
+registerForwardButton.onclick = () => {
     location.href = "/account/register";
-}
-
-const passwordfind = document.querySelector(".find-password-link");
-const accountCurrent = document.querySelector(".account-current");
-const accountPasswordForgot = document.querySelector(".account-password-forgot");
-
-passwordfind.onclick = () => {
-    accountCurrent.classList.add("account-invisible");
-    accountPasswordForgot.classList.remove("account-invisible");
 }
